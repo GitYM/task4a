23c2dea7d0e58e4fc81c5e12cac05f67dcb238fe
y_mohamed18 <y_mohamed18@cranford.hounslow.sch.uk>
2024-03-08 12:24:21 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+def convert_GDP_to_USD(local_currency):
+
+    """ this code will convert currency"""
+    amount_USD = local_currency * 1.26
+
+    return amount_USD
+
+# add function below to convert GDP to USD
+
+def convert_USD_to_GDP(currency_local):
+
+    """This code will convert chosen currency"""
+
+    amount_USD = currency_local / 0.79
+
+    return amount_USD
+
+
+
